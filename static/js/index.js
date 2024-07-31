8434dc22d859c8962602553d89a40ca147fbb121
muhammad ismail <muhismailmks@gmail.com>
2024-07-31 11:24:30 +0800

    update

--- static/js/index.js
+++ static/js/index.js
@@ -12,26 +12,49 @@ menu_btn.addEventListener('click', () => {
     menu_btn.classList.toggle('active')
     menu_drop.classList.toggle('active')
 })
- 
-close_menu.addEventListener('click', () => { 
+
+close_menu.addEventListener('click', () => {
     menu_drop.classList.remove('active')
 })
- 
 
 
-      // swiper js
-      const swiper1 = new Swiper('#swiper1', { 
-        direction: 'horizontal',
-        loop: true,
-        
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
 
-        // Menambahkan efek fade
-        effect: 'fade',
-        fadeEffect: {
-            crossFade: true
-        }
+// swiper js
+const swiper1 = new Swiper('#swiper1', {
+    direction: 'horizontal',
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // Menambahkan efek fade
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    }
+});
+
+
+// copy address
+
+const copyAddress = document.querySelector('.copy-box');
+let text = document.querySelector('.copy-box__text');
+let btn = document.querySelector('.copy-box__btn');
+let btnText = btn.textContent;
+let timeout;
+
+copyAddress.addEventListener('click', () => {
+    navigator.clipboard.writeText(text.textContent).then(function () {
+        btn.textContent = 'Copied';
+
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+            btn.textContent = btnText;
+        }, 2000);
+    }).catch(function (err) {
+        console.error('Failed to copy text: ', err);
     });
+
+})
